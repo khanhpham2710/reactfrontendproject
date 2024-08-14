4bc45948560ff24c30dafcf1a6844b9adde94c99
Khanh Pham <phamduykhanh271098@gmail.com>
2024-08-14 13:27:04 +0700

    fourthteen

--- src/components/Media/Media.js
+++ src/components/Media/Media.js
@@ -4,9 +4,7 @@ import Aos from "aos";
 
 function Media({ heading, content, right, image, youtubeId }) {
     useEffect(() => {
-        Aos.init({
-            duration: 1000,
-        });
+        Aos.init({ duration: 1000 });
     }, []);
 
     const imgRef = useRef(null);
@@ -37,61 +35,15 @@ function Media({ heading, content, right, image, youtubeId }) {
     }, [imgDimensions]);
 
     return (
-        <Container maxWidth="lg">
-            <Grid container columnSpacing={2}>
+        <Container maxWidth="md" sx={{ overflow: 'hidden' }}>
+            <Grid container columnSpacing={2} rowSpacing={4}>
                 <Grid item xs={12} md={7}
                     data-aos="fade-right"
                     sx={{
-                        display: {
-                            xs: "none",
-                            md: right ? "none" : "block"
-                        },
-                        position: 'relative'
+                        display: { xs: "none", md: right ? "none" : "block" },
+                        position: 'relative',
+                        overflow: 'hidden',
                     }}>
-                    <Box
-                        sx={{
-                            position: 'relative',
-                            width: '100%',
-                            height: '100%',
-                            display: 'flex',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <img
-                            ref={imgRef}
-                            src={image}
-                            alt={heading}
-                            style={{
-                                width: '100%',
-                                height: "100%",
-                                objectFit: 'contain',
-                                position: 'relative',
-                                zIndex: 0
-                            }}
-                        />
-                    </Box>
-                </Grid>
-                <Grid item xs={12} md={5} 
-                data-aos={right?"fade-right":"fade-left"}
-                sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    flexDirection: "column"
-                }}>
-                    <Typography variant='h3' textAlign="center" mb={6}>{heading}</Typography>
-                    <Typography variant='h5' textAlign="center">{content}</Typography>
-                </Grid>
-                <Grid item xs={12} md={7} 
-                data-aos="fade-left"
-                sx={{
-                    display: {
-                        xs: "block",
-                        md: right ? "block" : "none"
-                    },
-                    position: 'relative'
-                }}>
                     <Box
                         sx={{
                             position: 'relative',
@@ -114,12 +66,54 @@ function Media({ heading, content, right, image, youtubeId }) {
                                 zIndex: 0
                             }}
                         />
-                        <iframe
+                    </Box>
+                </Grid>
+                <Grid item xs={12} md={5}
+                    data-aos={right ? "fade-right" : "fade-left"}
+                    sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        flexDirection: "column"
+                    }}>
+                    <Typography variant='h3' textAlign="center" mb={6}>{heading}</Typography>
+                    <Typography variant='h5' textAlign="center">{content}</Typography>
+                </Grid>
+                <Grid item xs={12} md={7}
+                    data-aos="fade-left"
+                    sx={{
+                        display: { xs: "block", md: right ? "block" : "none" },
+                        position: 'relative',
+                        overflow: 'hidden',
+                    }}>
+                    <Box
+                        sx={{
+                            position: 'relative',
+                            width: '100%',
+                            height: '100%',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <img
+                            ref={imgRef}
+                            src={image}
+                            alt={heading}
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                objectFit: 'contain',
+                                position: 'relative',
+                                zIndex: 0
+                            }}
+                        />
+                        {youtubeId && <iframe
                             ref={iframeRef}
                             src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1&loop=1&playlist=${youtubeId}&mute=1&controls=0&showinfo=0&rel=0`}
-                            frameborder="0"
+                            frameBorder="0"
                             allow="autoplay; encrypted-media"
-                            allowfullscreen
+                            allowFullScreen
                             style={{
                                 position: 'absolute',
                                 top: '30%',
@@ -130,7 +124,7 @@ function Media({ heading, content, right, image, youtubeId }) {
                                 objectFit: 'cover',
                                 zIndex: -1
                             }}
-                        />
+                        />}
                     </Box>
                 </Grid>
             </Grid>
