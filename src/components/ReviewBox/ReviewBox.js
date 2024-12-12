06d58dc629a151992b18e0b7aa5fd2adccf2c0d0
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-12-12 17:22:44 +0700

    fix review box

--- src/components/ReviewBox/ReviewBox.js
+++ src/components/ReviewBox/ReviewBox.js
@@ -41,7 +41,7 @@ const Textarea = styled(BaseTextareaAutosize)(
 export default function ReviewBox({id}) {
   const [review, setReview] = React.useState('');
   const dispatch = useDispatch()
-  const { currentUser } = useAuth()
+  const { currentUser, userLoggedIn } = useAuth()
 
   function handleSubmit() {
     dispatch(addReviews({id,review, photoURL: currentUser.photoURL, user: {
